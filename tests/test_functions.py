f586d687fc2c5c47476b1ac7d16aabb8c8bac901
Ryan Delaney <ryan.delaney@gmail.com>
2016-11-03 11:41:48 -0700

    eval_absolute returns numbers, not text

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -9,12 +9,12 @@ class test_eval_absolute(unittest.TestCase):
 
     def test_white_unpadded(self):
         result = annotator.eval_absolute(10.01, True)
-        self.assertEqual(result, '10.01')
+        self.assertEqual(result, 10.01)
 
     def test_white_padded(self):
         result = annotator.eval_absolute(10, True)
-        self.assertEqual(result, '10.00')
+        self.assertEqual(result, 10.00)
 
     def test_black(self):
         result = annotator.eval_absolute(10.00, False)
-        self.assertEqual(result, '-10.00')
+        self.assertEqual(result, -10.00)
