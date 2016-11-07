0c2703355343bd108bf74c6ba2723a92fea651d7
Max Brustkern <max@canonical.com>
2016-11-07 14:05:28 -0500

    Adding assertRaises example

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import MagicMock
 
 import annotator
 
@@ -18,3 +19,14 @@ class test_eval_absolute(unittest.TestCase):
     def test_black(self):
         result = annotator.eval_absolute(10.00, False)
         self.assertEqual(result, '-10.00')
+
+
+class test_eval_numeric(unittest.TestCase):
+
+    def test_raises_runtimeerror(self):
+        score = MagicMock()
+        score.mate = None
+        score.cp = None
+        info_handler = MagicMock()
+        info_handler.info = {'score': [None, score]}
+        self.assertRaises(RuntimeError, annotator.eval_numeric, info_handler)
