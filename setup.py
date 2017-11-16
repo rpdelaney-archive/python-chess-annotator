dd9fdc0a74af3b2701909c3228644dba70cdc88d
Ryan Delaney <ryan.delaney@gmail.com>
2017-11-15 20:51:05 -0800

    Remove cruft

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'sample=sample:main',
+            'annotator=annotator.__main__:main',
         ],
     },
 )
