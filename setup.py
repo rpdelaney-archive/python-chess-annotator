3313799b19c3f3dc9cddf142b8978785e624fb6c
Ryan Delaney <ryan.delaney@gmail.com>
2017-11-16 17:00:20 -0800

    Update version to 1.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     description='adds comments and analysis to chess games',
     long_description=long_description,
 
-    version='1.0',
+    version='1.0.1',
 
     # The project's main homepage.
     url='https://github.com/rpdelaney/python-chess-annotator',
