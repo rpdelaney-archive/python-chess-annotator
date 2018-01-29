efcb0dd0646e0392729caa8696315cda662d512a
Ryan Delaney <rydelane@amazon.com>
2018-01-29 12:49:31 -0800

    Add MANIFEST.in to include eco.json in package #5
    Per http://python-packaging.readthedocs.io/en/latest/non-code-files.html

--- setup.py
+++ setup.py
@@ -82,9 +82,10 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'eco': ['annotator/eco/eco.json'],
-    },
+#   package_data={
+#       'eco': ['annotator/eco/eco.json'],
+#   },
+    include_package_data=True,
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
