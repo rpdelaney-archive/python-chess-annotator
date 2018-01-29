3bbe8103a1ef347180a260c36443ff531ff4f2ab
Ryan Delaney <rydelane@amazon.com>
2018-01-29 11:26:46 -0800

    Update path to eco submodule for package_data

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'eco': ['eco/eco.json'],
+        'eco': ['annotator/eco/eco.json'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
