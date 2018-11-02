f74136c6198e7ab730edd41bde79b67ce87ea56b
Ryan Delaney <ryan.delaney@gmail.com>
2018-11-02 16:09:33 -0700

    Use better module docstrings

--- annotator/__main__.py
+++ annotator/__main__.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-"""
-© Copyright 2016-2018 Ryan Delaney. All rights reserved.
-This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your
-option) any later version.
-
-You should have received a copy of the GNU General Public License along
-with this program.  If not, see <http://www.gnu.org/licenses/>.
+__author__ = "Ryan Delaney (ryan DOT delaney AT gmail DOT com)"
+__copyright__ = """© Copyright 2016-2018 Ryan Delaney. All rights reserved.
+ This work is distributed WITHOUT ANY WARRANTY whatsoever; without even the
+ implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the README file for additional terms and conditions on your use of this
+ software.
 """
 
 import os
