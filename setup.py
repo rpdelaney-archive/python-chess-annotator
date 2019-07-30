be5688006fc51f720c9864609805e8938a8377c0
Ryan Delaney <ryan.delaney@gmail.com>
2019-07-30 14:57:32 -0700

    Pin python-chess version on 0.27.3

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['python-chess'],
+    install_requires=['python-chess==0.27.3'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
