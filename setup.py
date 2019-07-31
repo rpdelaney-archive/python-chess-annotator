2d17daea286c9639830ee8511e603db98ff10aa3
Ryan Delaney <ryan.delaney@gmail.com>
2019-07-31 15:04:02 -0700

    Add pytest as test extra

--- setup.py
+++ setup.py
@@ -87,10 +87,10 @@ setup(
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-#   extras_require={
-#       'dev': ['check-manifest'],
-#       'test': ['coverage'],
-#   },
+    extras_require={
+#       'dev': ['black'],
+        'test': ['pytest', 'pytest-cov'],
+    },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
